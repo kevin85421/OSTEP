c0e3585464e2d6adb123298ca756746a9a95f71f
Kai-Hsun Chen <b03901153@ntu.edu.tw>
2021-06-05 14:39:51 +0800

    Example: how to run user program in xv6

--- xv6-wisc/user/makefile.mk
+++ xv6-wisc/user/makefile.mk
@@ -16,7 +16,8 @@ USER_PROGS := \
 	tester\
 	usertests\
 	wc\
-	zombie
+	zombie\
+	myprogram 
 
 USER_PROGS := $(addprefix user/, $(USER_PROGS))
 
---
+++ xv6-wisc/user/myprogram.c
@@ -0,0 +1,10 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+ 
+int
+main(void)
+{
+  printf(1, "My first xv6 program\n");
+  exit();
+}
